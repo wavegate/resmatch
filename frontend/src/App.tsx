54444e7381a277ac32ff5a5ef6afe1f8272f8dcc
Frank Lee <flee63@gatech.edu>
2024-08-02 22:33:24 -0700

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import "./App.css";
 
+const localBackend = "http://localhost:3000/";
+const prodBackend = "https://powerful-falls-38746-ef92b126204d.herokuapp.com/";
+
 function App() {
   useEffect(() => {
-    fetch("https://powerful-falls-38746-ef92b126204d.herokuapp.com/")
+    fetch(prodBackend + "first-user")
       .then((res) => res.json())
       .then(console.log);
   }, []);
