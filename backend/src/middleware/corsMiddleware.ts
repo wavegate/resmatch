aac00daef7eccff70d82184ba9cde5237c056ec2
Frank Lee <flee63@gatech.edu>
2024-08-12 23:31:22 -0700

    heroku deploy

--- backend/src/middleware/corsMiddleware.ts
+++ backend/src/middleware/corsMiddleware.ts
@@ -5,14 +5,18 @@ const allowedOrigins = [
   "https://www.residencymatch.net",
 ];
 
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); // Allow the request
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", // Allow all origins
 };
 
 export default corsOptions;
