b140318810b530f86f94720df9e4b89a0ad3b563
Frank Lee <flee63@gatech.edu>
2024-08-17 01:47:49 -0700

    heroku deploy

--- backend/seed/delete.ts
+++ backend/seed/delete.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function deleteAllReplies() {
-  await prisma.city.deleteMany();
+  await prisma.bin.deleteMany();
   console.log("All replies have been deleted.");
 }
 
