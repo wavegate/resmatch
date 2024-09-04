8b5606f6324e385931adc635c897e71005377f21
Frank Lee <flee63@gatech.edu>
2024-09-03 22:54:10 -0700

    commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -122,6 +122,10 @@ export default () => {
           >
             <h4 className={`font-medium text-xl`}>Changelog</h4>
             <div className={`flex flex-col gap-1`}>
+              <div className={`font-medium`}>9/3/2024</div>
+              <div>Schedule deatils imported</div>
+            </div>
+            <div className={`flex flex-col gap-1`}>
               <div className={`font-medium`}>8/26/2024</div>
               <div>Missing programs imported, connected to cities</div>
             </div>
